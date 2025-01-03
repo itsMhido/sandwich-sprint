5a5954470410468153eba13f3c0cc8749b673b6f
itsMhido <bahrimohammedmehdi@gmail.com>
2025-01-03 02:16:15 +0100

    Update achievementManagement.js

--- scripts/achievementManagement.js
+++ scripts/achievementManagement.js
@@ -6,12 +6,10 @@ function addBlock(achievement) {
     let id = achievement.id;
     let unlocked = achievement.unlocked;
     
-    // Create a new mini block
     const miniBlock = document.createElement('div');
     miniBlock.className = 'mini-block';
     miniBlock.id = id
   
-    // Create the first small block (image container)
     const smallBlock1 = document.createElement('div');
     smallBlock1.className = 'small-block1';
   
@@ -22,7 +20,6 @@ function addBlock(achievement) {
     }
     smallBlock1.appendChild(img);
   
-    // Create the second small block (text container)
     const smallBlock2 = document.createElement('div');
     smallBlock2.className = 'small-block2';
   
@@ -35,11 +32,9 @@ function addBlock(achievement) {
     smallBlock2.appendChild(titleElement);
     smallBlock2.appendChild(textElement);
   
-    // Append small blocks to the mini block
     miniBlock.appendChild(smallBlock1);
     miniBlock.appendChild(smallBlock2);
   
-    // Append the mini block to the big block container
     bigBlock.appendChild(miniBlock);
 }
   
@@ -104,40 +99,32 @@ function addAchievement(achievement) {
 
 function createAchievementNotif(achievement, text) {
 
-    // Create the sliding block element
     const block = document.createElement('div');
     block.classList.add('sliding-block');
 
-    // Create the image element
     const img = document.createElement('img');
     img.src = achievement.img;
     
-    // Create the text container
     const textContainer = document.createElement('div');
     textContainer.classList.add('text');
     textContainer.innerText = text;
 
-    // Append the image and text to the block
     block.appendChild(img);
     block.appendChild(textContainer);
 
-    // Append the block to the sliding container
     const container = document.querySelector('.sliding-container');
     container.appendChild(block);
 
-    // Show the block with the slide-in effect
     setTimeout(() => {
         block.classList.add('show');
-    }, 10); // Ensure class is added after render
+    }, 10);
 
-    // After a few seconds, hide the block with slide-out effect
     setTimeout(() => {
         block.classList.remove('show');
-    }, 3000); // Block slides out after 5 seconds
+    }, 3000);
 
-    // Remove the block from the DOM after it has slid out
     setTimeout(() => {
         block.remove();
-    }, 4000); // Wait for the block to slide out completely before removing it
+    }, 4000);
 }
 
